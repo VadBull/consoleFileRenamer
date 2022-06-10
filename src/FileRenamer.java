8f6cde9ae8080cbeb63238c878b47130b2086628
VadBull <wegonetakethepowerback@gmail.com>
2022-06-10 13:36:51 +0600

    Space

--- src/FileRenamer.java
+++ src/FileRenamer.java
@@ -55,6 +55,7 @@ public class FileRenamer {
                 }
             }
             filenameCounter++;
+
             renamer.setLength(0);
             renamer.append(path);
             renamer.append(fileName);
